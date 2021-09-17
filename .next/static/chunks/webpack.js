6b484390293542730b0f02b1d161590db4cb534c
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-09-17 14:33:34 +0200

    update next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a6ef1ea440ca0f502967";
+/******/ 	var hotCurrentHash = "94ec0cc05dceb371f143";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
