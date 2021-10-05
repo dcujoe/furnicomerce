75392e064161a407615e6d73859cc8a96c0177b0
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-10-05 15:48:15 +0200

    udpate images

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b70d734f802c356b978"; }
+/******/ 		__webpack_require__.h = function() { return "b857ecf7491fec771e1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
