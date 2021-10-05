3b64e9177e78f8f827d13f97d35faf7b412a2ce7
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-10-05 19:43:34 +0200

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c357e44dddb6d344c9f"; }
+/******/ 		__webpack_require__.h = function() { return "4029e293bf46a14fe24a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
