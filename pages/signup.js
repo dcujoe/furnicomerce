3d94881861b189765b7910433db25314ecfa385e
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-10-13 09:35:10 +0200

    signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,28 @@
+import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react'
+
+
 function Signup() {
-  return <>signup</>;
+  return <>
+  <Message 
+   attached
+   icon="settings"
+   header="Get Started"
+   content="Create a new account..."
+   color="teal"
+  />
+  <Form>
+    <Segment>
+      <Form.Input
+       fluid
+       icon="user"
+       iconPosition="left"
+       label="Name"
+       placeholder="Name"
+       name="name"
+       />
+    </Segment>
+  </Form>
+  </>;
 }
 
 export default Signup;
