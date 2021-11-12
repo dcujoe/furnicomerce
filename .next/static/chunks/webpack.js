dd3eb76a9d17d20faa2e05d69a718e3812c48725
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-11-12 23:48:33 +0100

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "095b00d5921785a02c0e"; }
+/******/ 		__webpack_require__.h = function() { return "c2da687385f9c4dffa87"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl = 
     process.env.NODE_ENV === "production" 
-    ? 'https://deployment-url.now.sh'
+    ? 'https://deployment-url.now'
     : 'http://localhost:3000' 
 
 
