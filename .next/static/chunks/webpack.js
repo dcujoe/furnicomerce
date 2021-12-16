b6afe5166001e4b214b5d329bfeab3f90bf67874
Daniel Agbenyega <dcujoe@gmail.com>
2021-12-16 22:37:41 +0100

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18331abb1dd361778219"; }
+/******/ 		__webpack_require__.h = function() { return "1939484efa601e380d5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
