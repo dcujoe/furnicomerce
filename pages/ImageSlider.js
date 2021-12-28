91dbf0785be0f9832b7398d8fda6637557552554
Daniel Agbenyega <dcujoe@gmail.com>
2021-12-28 22:50:03 +0100

    udpates

--- pages/ImageSlider.js
+++ pages/ImageSlider.js
@@ -3,8 +3,25 @@ import { Link, Image } from 'semantic-ui-react'
 
 function ImageSlider() {
     return (
-    <>
-
-    </>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About Us</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog/hello-world">
+            <a>Blog Post</a>
+          </Link>
+        </li>
+      </ul>
     )
-}
+  }
+
+  export default ImageSlider;
+  
