cc988a36cf29e8487d53032ae2cf0efd1faae94e
Daniel Agbenyega <dcujoe@gmail.com>
2021-12-30 21:10:51 +0100

    updates

--- pages/ImageSlider.js
+++ pages/ImageSlider.js
@@ -1,28 +1,37 @@
-import { Link, Image } from 'semantic-ui-react';
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
+const slideImages = [
+  {
+    url: 'images/slide_2.jpg',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'images/slide_3.jpg',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'images/slide_4.jpg',
+    caption: 'Slide 3'
+  },
+];
 
-
-function ImageSlider() {
+const Slideshow = () => {
     return (
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About Us</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog/hello-world">
-            <a>Blog Post</a>
-          </Link>
-        </li>
-      </ul>
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
     )
-  }
+}
 
-  export default ImageSlider;
+export default Slideshow;
   
