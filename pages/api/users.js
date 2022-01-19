1f0f02db520e1d1ea0dd39f1aab63c92d155c972
Daniel Agbenyega <dcujoe@gmail.com>
2022-01-19 23:29:29 +0100

    practice

--- pages/api/users.js
+++ pages/api/users.js
@@ -14,3 +14,16 @@ export default async (req, res) => {
 
     }
 }
+
+
+//sample code practices
+export default async (req, res) => {
+    try {
+        const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
+        const users = await User.find({ _id: { $ne: userId } }).sort({ role: "asc" });
+        res.status(200).json(users)
+    } catch (error) {
+        console.error(error);
+        res.status(403).send("Please login again");
+    } 
+}
